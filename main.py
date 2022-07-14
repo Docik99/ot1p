32dbce21ed68b49b12904967bfbc9a676fb8cfc8
docik <docik@1.ru>
2022-07-15 00:05:57 +0300

    add def create_index

--- main.py
+++ main.py
@@ -10,7 +10,62 @@ def connect_elasticsearch(host, port):
         print('Not connect!')
     return es
 
+
+def create_index(index_name):
+    created = False
+    body_books = {
+        "settings": {
+            "analysis": {
+                "filter": {
+                    "russian_stop": {
+                        "type": "stop",
+                        "stopwords": "_russian_"
+                    },
+                    "russian_keywords": {
+                        "type": "stop",
+                        "stopwords": ["князь", "повезет", "сорок"]
+                    }
+                },
+
+                "analyzer": {
+                    "custom_analyzer": {
+                        "type": "custom",
+                        "tokenizer": "standard",
+                        "filter": [
+                            "lowercase",
+                            "russian_stop",
+                            "russian_keywords"
+                        ]
+                    }
+                }
+            }
+        },
+        "mappings": {
+            "document": {
+                "properties": {
+                    "title": {
+                        "type": "text",
+                        "analyzer": "standard",
+                        "search_analyzer": "standard"
+                    },
+                    "author": {
+                        "type": "text",
+                        "analyzer": "standard",
+                        "search_analyzer": "standard"
+                    },
+                    "year_publication": {
+                        "type": "date",
+                        "format": "yyyy"
+                    },
+                    "text": {
+                        "type": "text",
+                        "analyzer": "custom_analyzer",
+                        "search_analyzer": "custom_analyzer"
+                    }
+                }
+            }
+        }
+    }
+
 if __name__ == '__main__':
   connect_elasticsearch('localhost', 9200)
-  res = requests.get('http://localhost:9200')
-  print(res.content)
